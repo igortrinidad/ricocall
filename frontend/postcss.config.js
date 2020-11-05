2dd792a13f5b8ff61addd5617017a24f195fc676
Igor Trindade <igorlucast@hotmail.com>
2020-11-04 23:54:43 -0300

    Fixed doubled slash on env url

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,30 +1,36 @@
 const tailwindcss = require("tailwindcss");
 const autoprefixer = require("autoprefixer");
+const path = require('path');
 
 const purgecss = require('@fullhuman/postcss-purgecss')({
 
   extensions: ['html', 'js', 'php', 'vue'],
 
-  // Specify the paths to all of the template files in your project 
+  // Specify the paths to all of the template files in your project
   content: [
     './src/**/*.html',
     './src/**/*.vue',
     './src/**/**/*.vue',
     './src/**/**/**/*.vue',
     './src/**/**/**/**/*.vue',
-    './src/**/*.js'
+    './src/**/*.js',
+    path.join(__dirname, 'node_modules/izitoast/**/**/*.js')
   ],
 
   // Include any special characters you're using in this regular expression
   defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
 
   whitelistPatterns: [
+    /iziToast$/,
+    /iziToast/,
     //For vue transitions
     /^slide-enter-active/,
     /^slide-leave-active/,
     /^slide-enter/,
     /^slide-leave-to/,
-  ]
+  ],
+
+  whitelistPatternsChildren: [/iziToast$/],
 
 })
 
